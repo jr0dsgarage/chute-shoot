4df855619d7d58eab540bde2f1f1f7b227526f6d
j__r0d <jkozeal@gmail.com>
2023-03-05 21:57:16 -0700

    Refined edge detection logic

--- ship_CharacterBody2D.cs
+++ ship_CharacterBody2D.cs
@@ -6,9 +6,15 @@ public partial class ship_CharacterBody2D : CharacterBody2D
     [Export]
     public int Speed { get; set; } = 600;
 
+	public float shipSize;
+	public float bottomClamp;
+
     public void GetInput()
     {
-        if (480 < Position.Y || Position.Y < 0)
+		shipSize = (GetNode<CollisionShape2D>("ship_CollisionShape2D").Shape.GetRect().Size.X);
+		bottomClamp = 480 - shipSize;
+		// topClamp = shipSize;
+        if (bottomClamp < Position.Y || Position.Y < shipSize)
         {
             Velocity = Velocity with { Y = -Velocity.Y };
         }
