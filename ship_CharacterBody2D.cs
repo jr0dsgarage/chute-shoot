0bebe6f4201076d9475eab50a7394bfad76abfbe
j__r0d <jkozeal@gmail.com>
2023-03-10 20:22:03 -0700

    encapsulated speed and shipSize; move assigns out of GetInput

--- ship_CharacterBody2D.cs
+++ ship_CharacterBody2D.cs
@@ -1,31 +1,32 @@
 using Godot;
-using System;
 
 public partial class ship_CharacterBody2D : CharacterBody2D
 {
     [Export]
-    public int Speed { get; set; } = 600;
+    public int Speed { get => speed; set => speed = value; }
+    public float ShipSize { get => shipSize; set => shipSize = value; }
 
-    private float shipSize;
     private float bottomClamp;
+    private int speed = 200;
+    private float shipSize;
 
     public void GetInput()
     {
-        shipSize = GetNode<CollisionShape2D>("ship_CollisionShape2D").Shape.GetRect().Size.X;
-        bottomClamp = 480 - shipSize/2;
-
-        if (bottomClamp < Position.Y || Position.Y < shipSize/2) // topClamp = 0 + shipSize/2;
+        if (bottomClamp < Position.Y || Position.Y < shipSize / 2) // topClamp = 0 + shipSize/2;
         {
-            Velocity = Velocity with { Y = -Velocity.Y };
+            // TODO - Screen Edge Bouncing should use Collision objects in prep for chutes
+            Velocity = Velocity with { Y = -Velocity.Y }; 
         }
         else
         {
-            Velocity = Transform.X * Input.GetAxis("up", "down") * Speed;
+            Velocity = Transform.X * Input.GetAxis("up", "down") * speed;
         }
     }
 
     public override void _PhysicsProcess(double delta)
     {
+        shipSize = GetNode<CollisionShape2D>("ship_CollisionShape2D").Shape.GetRect().Size.X;
+        bottomClamp = 480 - shipSize / 2;
         GetInput();
         MoveAndSlide();
     }
