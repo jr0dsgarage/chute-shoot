e37e1257c4464de4dc6d8d298d4632f6f65f3891
j__r0d <jkozeal@gmail.com>
2023-03-11 21:46:05 -0700

    change bg_layer scroll speed to a float value for finer control

--- bgLayer.cs
+++ bgLayer.cs
@@ -4,9 +4,10 @@ using System;
 public partial class bgLayer : TextureRect
 {
 	[Export]
-	public int ScrollSpeed { get => _scrollSpeedMultiplier ; set => _scrollSpeedMultiplier = value; }
+	public float ScrollSpeed { get => _scrollSpeedMultiplier ; set => _scrollSpeedMultiplier = value; }
 
-	private int _scrollSpeedMultiplier = 1;
+	private float _scrollSpeedMultiplier = 1.0f;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -16,11 +17,12 @@ public partial class bgLayer : TextureRect
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		// bg_layers are twice the screen width and tiled 
 		if (Position.X >= -640)
 		{
 			Position = Position with { X = Position.X - (1.0f * _scrollSpeedMultiplier) };
 		}
-		if (Position.X == -640)
+		if (Position.X <= -640)
 		{
 			Position = Position with { X = 0.0f };
 		}
