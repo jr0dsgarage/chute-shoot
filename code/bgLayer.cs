e40d2cbba038e624f09ee624c075b66cf75a05bb
j__r0d <jkozeal@gmail.com>
2023-03-20 22:12:03 -0600

    update background behavior - add horizontal parallax boolean option - allows backmost bgLayer to ignore horizontal movement but possibly follow vertical changes

--- code/bgLayer.cs
+++ code/bgLayer.cs
@@ -7,8 +7,11 @@ public partial class bgLayer : TextureRect
 	public float ScrollSpeed { get => _scrollSpeedMultiplier * 10; set => _scrollSpeedMultiplier = value / 10; }
 	[Export]
 	public bool UseVerticalParallax { get => _verticalParallaxEnabled ; set => _verticalParallaxEnabled = value;}
+	[Export]
+	public bool UseHorizontalParallax { get => _horizontalParallaxEnabled ; set => _horizontalParallaxEnabled = value;}
 	private static ship_CharacterBody2D _shipNode;
 	private float _scrollSpeedMultiplier = 1.0f;
+	private bool _horizontalParallaxEnabled;
 	private bool _verticalParallaxEnabled;
 
 	// Called when the node enters the scene tree for the first time.
@@ -21,7 +24,7 @@ public partial class bgLayer : TextureRect
 	public override void _Process(double delta)
 	{
 		// bg_layers are twice the screen width and tiled, then scaled
-		if (Position.X >= -640 * Scale.X)
+		if (_horizontalParallaxEnabled && Position.X >= -640 * Scale.X ) 
 		{
 			Position = Position with { X = Position.X - (1.0f * _scrollSpeedMultiplier) };
 		}
